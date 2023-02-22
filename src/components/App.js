9f80e4b40984fdb3b5914aa99b64ab387d24a6a4
Maryam Muchai <maryammuchai@gmail.com>
2023-02-22 16:13:03 +0300

    add on useState React Hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from "./Header";
 import Addcontact from "./Addcontact";
 import ContactList from "./ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState([])
+  const addContactHandler = (contact) => {
+    console.log(contact);
+  }
 
-  const contacts = [
-  {
-    id: "1",
-    name: "Marya",
-    email: "marya@gmail.com",
-  },
-  {
-    id: "2",
-    name: "Maryam",
-    email: "marya2gmail.com",
-  },
-];
   return (
   <div className='ui container'>
     <Header />
-    <Addcontact />
+    <Addcontact addContactHandler = {addContactHandler} />
     <ContactList contacts={contacts}/>
   </div>
   );
